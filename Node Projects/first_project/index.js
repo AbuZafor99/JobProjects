8ce3711c0b65d20807335590369a0dbc507d3e8f
AbuZafor99 <zaforsadiq987@gmail.com>
2025-10-19 20:52:06 +0600

    commit with code

--- Node Projects/first_project/index.js
+++ Node Projects/first_project/index.js
@@ -1,2 +1,8 @@
-console.log("Hello, World!");
-    console.log("This is my first Node.js project.");
+// console.log('Hello, World!');
+// console.log('This is my first Node.js project.');
+// const arrayNumber = [1, 2, 3, 4, 5];
+// arrayNumber.forEach((num) => {
+//     console.log(`Number: ${num}`);
+// });
+class Salary {
+     
