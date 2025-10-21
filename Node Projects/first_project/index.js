3a1f23eb1e2c733bef72a63c37686682d96644ce
AbuZafor99 <zaforsadiq987@gmail.com>
2025-10-21 20:40:30 +0600

    server and request

--- Node Projects/first_project/index.js
+++
@@ -1,8 +0,0 @@
-// console.log('Hello, World!');
-// console.log('This is my first Node.js project.');
-// const arrayNumber = [1, 2, 3, 4, 5];
-// arrayNumber.forEach((num) => {
-//     console.log(`Number: ${num}`);
-// });
-class Salary {
-     
